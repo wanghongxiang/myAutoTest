f1e085d76567678bd1f7cfa41d9585f449fb5ddf
hp <hp@DESKTOP-2GBREN7>
2018-10-28 13:37:39 +0800

    测试一下 merge

---
+++ src/main/java/system/Init.java
@@ -0,0 +1,13 @@
+package system;
+
+public class Init {
+	DriverInfo appDriverInfo;
+	UserInfo appUserInfo;
+	UserInfo webUserInfo;
+	public Init(){
+		appDriverInfo = new DriverInfo();
+		appUserInfo = new UserInfo();
+		webUserInfo = new UserInfo();
+		appDriverInfo = new DriverInfo();
+	}
+}
---
+++ src/main/java/system/UserInfo.java
@@ -0,0 +1,36 @@
+package system;
+
+public class UserInfo {
+
+	Long cid;
+	Long uid;
+	String uname;
+	String passwd;
+	
+	
+	public Long getCid() {
+		return cid;
+	}
+	public void setCid(Long cid) {
+		this.cid = cid;
+	}
+	public Long getUid() {
+		return uid;
+	}
+	public void setUid(Long uid) {
+		this.uid = uid;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	
+}
